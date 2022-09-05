1f434e6f0301cd73723458b58b075b59b20dfe28
pascal <pascal@lbcstudios.ca>
2022-09-05 11:57:15 -0700

    styled code

--- part_2/app.R
+++ part_2/app.R
@@ -9,100 +9,102 @@ list.files(here::here("part_2/R")) %>%
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    br(),
-    br(),
+  shiny::includeCSS(here::here("part_2/www/scripts.js")),
 
-    div(id = "placeholder"),
-    shiny::tagList(
+  br(),
+  br(),
 
-        # first viz
+  div(id = "placeholder"),
+  shiny::tagList(
+
+    # first viz
+    div(
+      class = "class_a",
+      div(
+        class = "col-md-6",
         div(
-            class = "class_a",
+          class = "panel panel-default",
+          div(
+            class = "panel-heading clearfix",
+            tags$h2("Visualization 1", class = "pull-left panel-title"),
             div(
-                class = "col-md-6",
-                div(
-                    class = "panel panel-default",
-                    div(
-                        class = "panel-heading clearfix",
-                        tags$h2("Visualization 1", class = "pull-left panel-title"),
-                        div(
-                            class = "pull-right",
-                            shiny::actionButton(
-                                inputId = "a",
-                                label = "",
-                                class = "btn-danger delete",
-                                icon = shiny::icon("minus")
-                            )
-                        )
-                    ),
-                    div(
-                        class = "panel-body",
-                        plotly::plot_ly(mtcars, x = ~mpg, y = ~wt)
-                    )
-                )
+              class = "pull-right",
+              shiny::actionButton(
+                inputId = "a",
+                label = "",
+                class = "btn-danger delete",
+                icon = shiny::icon("minus")
+              )
             )
-        ),
-
-        # second viz
-        div(
-            class = "class_b",
-            div(
-                class = "col-md-6",
-                div(
-                    class = "panel panel-default",
-                    div(
-                        class = "panel-heading clearfix",
-                        tags$h2("Visualization 2", class = "pull-left panel-title"),
-                        div(
-                            class = "pull-right",
-                            shiny::actionButton(
-                                inputId = "b",
-                                label = "",
-                                class = "btn-danger delete",
-                                icon = shiny::icon("minus")
-                            )
-                        )
-                    ),
-                    div(
-                        class = "panel-body",
-                        plotly::plot_ly(mtcars, x = ~mpg, y = ~wt)
-                    )
-                )
-            )
-        ),
-
-        # third viz
-        div(
-            class = "class_c",
-            div(
-                class = "col-md-6",
-                div(
-                    class = "panel panel-default",
-                    div(
-                        class = "panel-heading clearfix",
-                        tags$h2("Visualization 3", class = "pull-left panel-title"),
-                        div(
-                            class = "pull-right",
-                            shiny::actionButton(
-                                inputId = "c",
-                                label = "",
-                                class = "btn-danger delete",
-                                icon = shiny::icon("minus")
-                            )
-                        )
-                    ),
-                    div(
-                        class = "panel-body",
-                        plotly::plot_ly(mtcars, x = ~mpg, y = ~wt)
-                    )
-                )
-            )
+          ),
+          div(
+            class = "panel-body",
+            plotly::plot_ly(mtcars, x = ~mpg, y = ~wt)
+          )
         )
-
-
+      )
     ),
 
-    shiny::includeScript(here::here("part_2/www/scripts.js"))
+    # second viz
+    div(
+      class = "class_b",
+      div(
+        class = "col-md-6",
+        div(
+          class = "panel panel-default",
+          div(
+            class = "panel-heading clearfix",
+            tags$h2("Visualization 2", class = "pull-left panel-title"),
+            div(
+              class = "pull-right",
+              shiny::actionButton(
+                inputId = "b",
+                label = "",
+                class = "btn-danger delete",
+                icon = shiny::icon("minus")
+              )
+            )
+          ),
+          div(
+            class = "panel-body",
+            plotly::plot_ly(mtcars, x = ~mpg, y = ~wt)
+          )
+        )
+      )
+    ),
+
+    # third viz
+    div(
+      class = "class_c",
+      div(
+        class = "col-md-6",
+        div(
+          class = "panel panel-default",
+          div(
+            class = "panel-heading clearfix",
+            tags$h2("Visualization 3", class = "pull-left panel-title"),
+            div(
+              class = "pull-right",
+              shiny::actionButton(
+                inputId = "c",
+                label = "",
+                class = "btn-danger delete",
+                icon = shiny::icon("minus")
+              )
+            )
+          ),
+          div(
+            class = "panel-body",
+            plotly::plot_ly(mtcars, x = ~mpg, y = ~wt)
+          )
+        )
+      )
+    )
+
+
+  ),
+
+  shiny::includeScript(here::here("part_2/www/scripts.js"))
 
 )
 
